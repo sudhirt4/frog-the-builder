6e55566eacca6afaa366167c4988d70cf7768a8c
leapfrog-saugat <saugatgautam@lftechnology.com>
2015-06-13 11:54:35 +0545

    Add tweens for oscillation

--- js/tower.js
+++ js/tower.js
@@ -1,9 +1,54 @@
 var game = new Phaser.Game(600, 600);
 // Play State
 var playState = {
-	create : function() {},
-	update : function() {},
-	preload : function() {},
+	create : function() {
+		this.block = game.add.sprite(game.world.centerX, 100, 'block');
+		this.block.anchor.setTo(0.5, 0.5);
+
+
+
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+		game.physics.arcade.enable(this.block);
+		this.block.collideWorldBounds = true;
+		this.block.checkCollision = { up: true, down: true, left: true, right: true };
+
+	    var tween = game.add.tween(this.block)
+	    .to({ x : 25, y : 25 }, 1000, Phaser.Easing.Linear.Out)
+	    .to({ x : game.world.centerX, y : 100 }, 1000, Phaser.Easing.Linear.Out)
+	    .to({ x : game.world.width - 25, y : 25 }, 1000, Phaser.Easing.Linear.Out)
+	    .to({ x : game.world.centerX, y : 100 }, 1000, Phaser.Easing.Linear.Out)
+	    .loop()
+	    .start();
+		console.log("Tween Move : ", tween);
+		
+		// Move the block from the keyboard
+		var space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		space.onDown.add(this.fall, this);
+	},
+	update : function() {
+		//Oscillate
+		// this.oscillate();
+		if (!(this.block.y < 600)) {
+			this.stopFall();
+		}
+	},
+	preload : function() {
+		game.load.image('block', 'images/block50.png');
+		game.load.image('bg', 'images/block100.png');
+	},
+	fall : function() {
+        game.tweens.removeAll();
+		this.block.body.gravity.y = 2000;
+	},
+	stopFall : function() {
+		console.log("Stopped");
+		this.block.body.gravity.y = -200;
+		console.log("Gravity of the block after calling stopFall: ",this.block.body.gravity.y);
+
+	},
+	oscillate : function() {
+		this.block.x++;
+	},
 	deathHandler : function() {}
 	};
 
