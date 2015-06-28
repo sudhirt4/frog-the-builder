065b2439f38dc48f60bef1d4d16603a2c8bc8ea2
sudhir shrestha <sudhirshrestha@live.com>
2015-06-28 15:08:34 +0545

    Refactored design

--- js/app.js
+++ js/app.js
@@ -1,74 +1,186 @@
-var game = new Phaser.Game(400, 400);
+var game = new Phaser.Game(400, 600);
 
-var fallingBlock;
-var topBlock;
+var score = 0;
 
 var playState = {
-  create: function() {
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+  create: function () {
+
+    game.world.setBounds(0, -1000, 400, 1600);
+
+    score = 0;
+    this.initY = 125;
+    var background = game.add.tileSprite(0, 0, 400, 600, 'background');
+    background.fixedToCamera = true;
+    var scoreboard = game.add.tileSprite(75, 50, 100, 50, 'block1');
+    scoreboard.anchor.setTo(0.5, 0.5);
+    scoreboard.fixedToCamera = true;
+    var scoreText = game.add.text(75, 50, 'score', {fill: '#fff'});
+    scoreText.anchor.setTo(0.5, 0.5);
+    scoreText.fixedToCamera = true;
+
+    var frog = game.add.tileSprite(game.world.width - 75, 550, 100, 100, 'frog');
+    frog.anchor.setTo(0.5, 0.5);
+    frog.fixedToCamera = true;
+
+    game.physics.startSystem(Phaser.Physics.P2JS);
+    game.physics.p2.setImpactEvents(true);
+    game.physics.p2.gravity.y = 250;
+
+    this.platform = game.add.sprite(game.world.centerX, 575, 'block');
+    this.platform.anchor.setTo(0.5, 0.5);
+    game.physics.p2.enable(this.platform);
+    this.platform.body.static = true;
 
     this.blocks = game.add.group();
-
     this.createBlock();
 
-    this.ground = game.add.sprite(game.world.centerX, 375, 'block1');
-    this.physics.arcade.enable(this.ground);
-    this.ground.anchor.setTo(0.5, 0.5);
-    this.ground.enableBody = true;
-    this.ground.body.immovable = true;
-    this.blocks.add(this.ground);
+    game.camera.x = 200;
+    game.camera.y = 200;
 
     var space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     space.onDown.add(this.fall, this);
+    game.camera.x = 500;
 
   },
-  createBlock: function() {
-    this.block = game.add.sprite(game.world.centerX, 100, 'block');
+  createBlock: function () {
+    this.block = game.add.sprite(game.world.centerX, this.initY, 'block');
+    this.block.swingStatus = true;
     this.block.anchor.setTo(0.5, 0.5);
-    game.physics.arcade.enable(this.block);
-    var tween = game.add.tween(this.block)
-    .to({ x : 25, y : 25 }, 1000, Phaser.Easing.Linear.Out)
-    .to({ x : game.world.centerX, y : 100 }, 1000, Phaser.Easing.Linear.Out)
-    .to({ x : game.world.width - 25, y : 25 }, 1000, Phaser.Easing.Linear.Out)
-    .to({ x : game.world.centerX, y : 100 }, 1000, Phaser.Easing.Linear.Out)
-    .loop()
-    .start();
-  },
-  fall: function() {
-    game.tweens.removeAll();
-    this.block.body.gravity.y = 1000;
-  },
-  update: function() {
-    var self = this;
-
-    game.physics.arcade.collide(this.block, this.blocks, function() {
-      self.block.body.gravity.y = 0;
-      self.block.enableBody = true;
-      self.block.body.immovable = true;
-      self.blocks.add(self.block);
-      self.createBlock();
-
-      if(self.blocks.length > 2) {
-        self.blocks.forEach(function(_block){
-          var _y = (_block.y + _block.height + 20);
-          var demoTween = game.add.tween(_block).to({y:_y},1000);
-          demoTween.start();
-        });
+    game.physics.p2.enable(this.block);
+    this.block.body.velocity.y = 0;
+    this.block.body.velocity.x = 150;
+    this.block.body.onBeginContact.add(function (x) {
+      if (x == null) {
+        score = this.blocks.length;
+        game.state.start('gameOverState');
       }
-      console.log(this.blocks)
-    });
-
+    }, this);
+    console.log(this.blocks.length);
   },
-  preload: function() {
-    game.load.image('block', 'images/block50.png');
-    game.load.image('towerTop', 'images/block100.png');
-    game.load.image('bg', 'images/block100.png');
-    game.load.spritesheet('block1',  'images/block50.png', 50, 50);
+  fall: function () {
+    this.block.body.velocity.x = 0;
+    this.block.body.velocity.y = 0;
+    this.block.swingStatus = false;
+    this.blocks.add(this.block);
+    if (this.blocks.length > 4) {
+      this.initY -= 50;
+      var tween = game.add.tween(game.camera);
+      tween.to({y: (game.camera.y - 50)}, 1000, Phaser.Easing.Linear.Out);
+      tween.start();
+    }
+  },
+  update: function () {
+    this.updateBlockSwing();
+    if (this.block.swingStatus == false) {
+      if (this.block.body.y > this.initY + 200) {
+        this.createBlock();
+      }
+    }
+  },
+  updateBlockSwing: function () {
+    if (this.block.swingStatus == true) {
+      if (this.block.body.velocity.y >= 0) {
+        this.block.body.velocity.y = -6;
+      }
+      if (this.block.body.x > game.world.width - (game.world.width / 6)) {
+        this.block.body.velocity.x = -150;
+      }
+      if (this.block.body.x < (game.world.width / 6)) {
+        this.block.body.velocity.x = 150;
+      }
+    }
+  },
+  preload: function () {
+    game.load.image('block', 'images/block.png');
+    game.load.image('frog','images/frog.png')
+    game.load.image('background', 'images/background.png');
   }
 }
 
+var homeState = {
+  create: function () {
+    game.world.setBounds(0, 0, 400, 600);
+    game.add.tileSprite(0, 0, 400, 600, 'background');
+    var title = game.add.text(game.world.centerX, game.world.height / 4, "FROG THE BUILDER", {fill: '#222'});
+    title.anchor.setTo(0.5, 0.5);
+
+    var btn = game.add.text(game.world.centerX,
+    (game.world.height / 2) + (game.world.height / 12),
+    'Build', {fill: '#222'});
+    btn.anchor.setTo(0.5, 0.5);
+    btn.inputEnabled = true;
+    btn.events.onInputDown.add(function () {
+      game.state.start('playState');
+    }, this);
+
+    var btn = game.add.text(game.world.centerX,
+    (game.world.height / 2) + 2 * (game.world.height / 12),
+    'Highscores', {fill: '#222'});
+    btn.anchor.setTo(0.5, 0.5);
+    btn.inputEnabled = true;
+    btn.events.onInputDown.add(function () {
+      game.state.start('playState');
+    }, this);
+
+    var btn = game.add.text(game.world.centerX,
+    (game.world.height / 2) + 3 * (game.world.height / 12),
+    'Instructions', {fill: '#222'});
+    btn.anchor.setTo(0.5, 0.5);
+    btn.inputEnabled = true;
+    btn.events.onInputDown.add(function () {
+      game.state.start('playState');
+    }, this);
+  },
+  preload: function () {
+    game.load.image('background', 'images/background.png');
+  }
+};
+
+var gameOverState = {
+  create: function () {
+    game.world.setBounds(0, 0, 400, 600);
+    game.add.tileSprite(0, 0, 400, 600, 'wood');
+    var title = game.add.text(game.world.centerX, game.world.height / 4, "Game Over", {fill: '#222'});
+    title.anchor.setTo(0.5, 0.5);
+
+    game.add.text(game.world.centerX, game.world.centerY - 50, score, {fill: '#222'}).anchor.setTo(0.5,0.5);
+    var btn = game.add.text(game.world.centerX,
+    (game.world.height / 2) + (game.world.height / 12),
+    'Build', {fill: '#222'});
+    btn.anchor.setTo(0.5, 0.5);
+    btn.inputEnabled = true;
+    btn.events.onInputDown.add(function () {
+      game.state.start('playState');
+    }, this);
+
+    var btn = game.add.text(game.world.centerX,
+    (game.world.height / 2) + 2 * (game.world.height / 12),
+    'Highscores', {fill: '#222'});
+    btn.anchor.setTo(0.5, 0.5);
+    btn.inputEnabled = true;
+    btn.events.onInputDown.add(function () {
+      game.state.start('playState');
+    }, this);
+
+    var btn = game.add.text(game.world.centerX,
+    (game.world.height / 2) + 3 * (game.world.height / 12),
+    'Instructions', {fill: '#222'});
+    btn.anchor.setTo(0.5, 0.5);
+    btn.inputEnabled = true;
+    btn.events.onInputDown.add(function () {
+      game.state.start('playState');
+    }, this);
+
+  },
+  preload: function () {
+    game.load.image('wood', 'images/background.png');
+  }
+};
 
 game.state.add("playState", playState);
-game.state.start("playState");
+game.state.add("homeState", homeState);
+game.state.add("gameOverState", gameOverState);
+game.state.start("homeState");
+
 
 
